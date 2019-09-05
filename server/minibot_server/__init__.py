79b75b435b4bd7693754c235b8fac5ec191747e4
Brian Chin <brian.chin@gmail.com>
2019-09-05 09:22:41 -0700

    Separate app creation into its own module

--- server/minibot_server/__init__.py
+++ server/minibot_server/__init__.py
@@ -2,61 +2,11 @@ from .oauth import (AccountCreationManager, OAuthCallbackManager, OAuthClientInf
 
 from tornado import web, ioloop, httpclient, escape
 import asyncio
-import secrets
-import yaml
-import os
 
 from .config import (ReadConfig, MinibotConfig)
+from . import app
 
 from typing import Awaitable
-
-class OAuthRedirectHandler(web.RequestHandler):
-    _callback_manager: OAuthCallbackManager
-
-    def initialize(self, callback_manager: OAuthCallbackManager) -> None:
-        self._callback_manager = callback_manager
-
-    def get(self) -> None:
-        state = self.get_argument('state')
-        code = self.get_argument('code')
-        asyncio.create_task(self._callback_manager.complete(state, code))
-
-class StartAccountCreateHandler(web.RequestHandler):
-    _callback_manager: OAuthCallbackManager
-    _creation_manager: AccountCreationManager
-
-    def initialize(self, callback_manager: OAuthCallbackManager, creation_manager: AccountCreationManager) -> None:
-        self._callback_manager = callback_manager
-        self._creation_manager = creation_manager
-
-    async def post(self) -> None:
-        token = secrets.token_urlsafe(30)
-        (url, callback) = await self._callback_manager.start_auth()
-        await self._creation_manager.add_creation(token, callback)
-        self.write({
-            'state_token': token,
-            'auth_url': url,
-        })
-
-class CompleteAccountCreateHandler(web.RequestHandler):
-    _creation_manager: AccountCreationManager
-
-    def initialize(self, creation_manager: AccountCreationManager) -> None:
-        self._creation_manager = creation_manager
-
-    async def post(self) -> None:
-        token = self.get_argument('state_token')
-        result = await self._creation_manager.wait_result(token)
-        self.write(result)
-
-class HelloWorldHandler(web.RequestHandler):
-    def get(self) -> None:
-        value = self.get_argument('name', default=None)
-        if value is not None:
-            self.write("Hello, %s!" % value)
-        else:
-            self.write("Hello, World!")
-        self.finish()
 
 def MakeRealOAuthProvider(config: MinibotConfig) -> OAuthProvider:
     config = ReadConfig()
@@ -67,20 +17,11 @@ def MakeRealOAuthProvider(config: MinibotConfig) -> OAuthProvider:
     )
     return OAuthProviderImpl(client_info, TWITCH_PROVIDER)
 
-def MakeServer(provider: OAuthProvider) -> web.Application:
-    callbacks = OAuthCallbackManager(provider)
-    creations = AccountCreationManager()
-    return web.Application([
-        (r'/callback', OAuthRedirectHandler, dict(callback_manager=callbacks)),
-        (r'/account/create', StartAccountCreateHandler, dict(callback_manager=callbacks, creation_manager=creations)),
-        (r'/account/complete', CompleteAccountCreateHandler, dict(creation_manager=creations)),
-    ])
-
 async def TestAccountCreateExchange() -> None:
     config = ReadConfig()
     provider = MakeRealOAuthProvider(config)
-    app = MakeServer(provider)
-    app.listen(8080)
+    http_app = app.CreateApp(provider)
+    http_app.listen(8080)
     client = httpclient.AsyncHTTPClient()
     async def inner() -> None:
         first_response = await client.fetch("http://localhost:8080/account/create", method = "POST", body = "")
@@ -99,11 +40,10 @@ class DumbHandler(web.RequestHandler):
         self.finish(f'Hello, World!\nHeaders: {dict(self.request.headers)}')
 
 async def RunDumbServer() -> None:
-    config = ReadConfig()
-    app = web.Application([
+    http_app = web.Application([
         (r'/', DumbHandler)
     ])
-    app.listen(8080)
+    http_app.listen(8080)
 
 def main() -> None:
     def loop_start(fut: Awaitable[None]) -> None:
---
+++ server/minibot_server/app.py
@@ -0,0 +1,65 @@
+"""The primary Tornado application"""
+
+from tornado import web
+import asyncio
+import secrets
+
+from . import oauth
+
+class OAuthRedirectHandler(web.RequestHandler):
+    _callback_manager: oauth.OAuthCallbackManager
+
+    def initialize(self, callback_manager: oauth.OAuthCallbackManager) -> None:
+        self._callback_manager = callback_manager
+
+    def get(self) -> None:
+        state = self.get_argument('state')
+        code = self.get_argument('code')
+        asyncio.create_task(self._callback_manager.complete(state, code))
+
+class StartAccountCreateHandler(web.RequestHandler):
+    _callback_manager: oauth.OAuthCallbackManager
+    _creation_manager: oauth.AccountCreationManager
+
+    def initialize(self, callback_manager: oauth.OAuthCallbackManager, creation_manager: oauth.AccountCreationManager) -> None:
+        self._callback_manager = callback_manager
+        self._creation_manager = creation_manager
+
+    async def post(self) -> None:
+        token = secrets.token_urlsafe(30)
+        (url, callback) = await self._callback_manager.start_auth()
+        await self._creation_manager.add_creation(token, callback)
+        self.write({
+            'state_token': token,
+            'auth_url': url,
+        })
+
+class CompleteAccountCreateHandler(web.RequestHandler):
+    _creation_manager: oauth.AccountCreationManager
+
+    def initialize(self, creation_manager: oauth.AccountCreationManager) -> None:
+        self._creation_manager = creation_manager
+
+    async def post(self) -> None:
+        token = self.get_argument('state_token')
+        result = await self._creation_manager.wait_result(token)
+        self.write(result)
+
+class HelloWorldHandler(web.RequestHandler):
+    def get(self) -> None:
+        value = self.get_argument('name', default=None)
+        if value is not None:
+            self.write("Hello, %s!" % value)
+        else:
+            self.write("Hello, World!")
+        self.finish()
+
+def CreateApp(provider: oauth.OAuthProvider) -> web.Application:
+    """Creates the minibot server application."""
+    callbacks = oauth.OAuthCallbackManager(provider)
+    creations = oauth.AccountCreationManager()
+    return web.Application([
+        (r'/callback', OAuthRedirectHandler, dict(callback_manager=callbacks)),
+        (r'/account/create', StartAccountCreateHandler, dict(callback_manager=callbacks, creation_manager=creations)),
+        (r'/account/complete', CompleteAccountCreateHandler, dict(creation_manager=creations)),
+    ])
